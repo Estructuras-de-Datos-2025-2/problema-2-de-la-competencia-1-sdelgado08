85d9be946524dac9908dd7db0acd8acedfe69f84
sdelgado08 <sdelgado0805@javerianacali.edu.co>
2025-08-29 09:39:42 -0500

    problema2

--- main.c
+++ main.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(void)
-{
-    int n;
-    scanf("%d",&n);
-    // TODO: malloc n, leer arreglo
-    // TODO: imprimir en orden inverso separados por espacio
-    // TODO: free
+
+int main(){
+    int n, i;
+    int *p;
+
+    scanf("%d", &n);
+    p = (int *)malloc(n*sizeof(int)); 
+
+    //ARREGLO
+    for(i=0; i<n; i++){
+        scanf("%d", p+i);
+    }
+
+    //ARREGLO INVERTIDO
+    for(i = n-1; i >= 0; i--){
+        printf("%d ", *(p+i));
+    }
+
+    free(p);
     return 0;
 }
